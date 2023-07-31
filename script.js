44b6718db8ac2224f549b9fafe1636ab0e32d137
Alin Bumbes <alinbumbes@gmail.com>
2023-07-31 18:10:28 +0300

    some shit like make the svg bg fixed actually fit window but does not work on resize browser

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict'
+//select svg element
+const svgElem = document.querySelector('.svg-set-fills')
+
+//set css variable for svg element to be the largest of the two dimensions
+const setPropForSvg = () =>
+    ((window.innerHeight * 2) <= window.innerWidth) ?
+        svgElem.style.setProperty("--width-or-height-largest", window.innerWidth + "px") :
+        svgElem.style.setProperty("--width-or-height-largest", window.innerHeight * 2 + "px")
+
+setPropForSvg()
+
+svgElem.addEventListener("resize", () => setPropForSvg());
